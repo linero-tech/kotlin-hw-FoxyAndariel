f7382949ff26f141e7402f3dacff8c50e8f1439b
FoxyAndariel <jonathan.studying@gmail.com>
2023-01-31 19:16:50 +0100

    Task 4 Module 101 update

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -4,10 +4,10 @@ fun task4(): String {
 // Got help from a classmate to solve this.
     return """
         INPUT value of number x
-        IF x<y
+        IF x is less than y
         OUTPUT number y
         ELSE
-        IF y<x
+        IF y is less than x
         OUTPUT number x
     """
 }
