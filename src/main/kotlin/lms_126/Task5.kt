7a118c6f8607b7040269d1310fd27f619f915d09
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-13 13:08:44 +0100

    Task 5 module 101

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -1,6 +1,33 @@
 package lms_126
 
 fun task5(): String {
-
-    return TODO("Replace this 'TODO' with your solution")
+/* I'm Given  the number "X" Monday is 1 & Sunday is 7
+* since there only "7 days" in a week that means that  the nr 8 is going to be a 0 or and error */
+    return """
+       // Starting with 1 as monday
+       INPUT X
+       if val is 1
+       OUTPUT Monday
+       
+       if val is 2 
+       OUTPUT Tuesday
+       
+       if val is 3
+       OUTPUT Wednesday
+       
+       if val is 4
+       OUTPUT Thursday 
+         
+         if val is 5
+         OUTPUT Friday
+         
+         if val is 6
+         OUTPUT Saturday
+         
+         if val is 7
+         OUTPUT Sunday
+         
+         if val is 8
+         OUTPUT Error
+    """
 }
