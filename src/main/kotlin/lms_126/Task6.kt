be5016c519b98db0524bca848d7a314b351db0c3
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-26 11:48:12 +0100

    Update Task6.kt

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """ // I have the numbers from 1 to 100 and I should be able to use multiple by 5?!
+        
+        // I in put the numbers 100/5 = 20 I'll make this an OUTPUT further down.
+               
+        INPUT numbers 1 to 100
+        
+        IF using 1 then println(1)
+         IF using 2 then println(2)
+          IF using 3 then println(3) 
+        
+    """
 }
