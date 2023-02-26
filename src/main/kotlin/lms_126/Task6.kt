ee17140be8e18ea81d8bd1f3a6379bd333b9d083
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-26 12:01:59 +0100

    Update Task6.kt

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -8,16 +8,16 @@ fun task6(): String {
                
         INPUT numbers 1 to 100
         
-        IF using 1 then println(1)
-         IF using 2 then println(2)
-          IF using 3 then println(3) 
-        IF using 4 then println(4)
-         IF using 5 then println(5)
-          IF using 6 then println(6)
-           IF using 7 then println(7)
-            IF using 8 then println(8)
-             IF using 9 then println(9)
-              IF using 10 then println(10)
+      IF using 1 then println(1)
+       IF using 2 then println(2)
+        IF using 3 then println(3) 
+         IF using 4 then println(4)
+          IF using 5 then println(5)
+           IF using 6 then println(6)
+            IF using 7 then println(7)
+             IF using 8 then println(8)
+              IF using 9 then println(9)
+               IF using 10 then println(10)
                
              IF using 100/5 println(20)
                
