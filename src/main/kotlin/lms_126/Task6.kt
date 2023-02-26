0185fbfea2ab94a3f4dea074df482dc2a810b6dd
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-26 12:04:04 +0100

    Task6 Mod 101 update

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -5,6 +5,7 @@ fun task6(): String {
     return """ // I have the numbers from 1 to 100 and I should be able to use multiple by 5?!
         
         // I in put the numbers 100/5 = 20 I'll make this an OUTPUT further down.
+           // should I put in  multiplication and/or division?
                
         INPUT numbers 1 to 100
         
