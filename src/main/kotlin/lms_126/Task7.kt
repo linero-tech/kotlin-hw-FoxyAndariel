69e07a0f0bc2ed1fe5b0481bffd1a5c4f0bd165b
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-27 13:25:45 +0100

    Task 7 module 101

--- src/main/kotlin/lms_126/Task7.kt
+++ src/main/kotlin/lms_126/Task7.kt
@@ -2,5 +2,16 @@ package lms_126
 
 fun task7(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        
+        // again I am given the numbers from 1 to 100
+        INPUT numbers 1 to 100
+          
+         IF val is 1 = println(1)
+         IF val is 2 = println(2)
+         IF val is 3 = println(3)
+         IF val is 4 = println(4)
+        ...
+        
+    """
 }
