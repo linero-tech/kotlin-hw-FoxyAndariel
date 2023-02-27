6efc53d8089a36f1e4fe78249a20355b7a0903c3
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-27 20:47:07 +0100

    Task 9 module 101

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT card to ATM
+        INPUT card code
+        INPUT amount to take out
+        IF amount is > balance OUTPUT error
+      ELSE if amount is < or the same
+      OUTPUT  chosen amount
+      OUTPUT payslip & card to user 
+    """
 }
