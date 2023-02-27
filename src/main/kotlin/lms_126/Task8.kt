58e372db306f899120a7f425761a6e22cfb539eb
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-27 18:44:27 +0100

    Task8 module 101

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,5 +2,25 @@ package lms_126
 
 fun task8(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        /*If X is multiple of 3, print Foo 
+        If X is multiple of 5, print Bar
+        If X is multiple of both 3 and 5, print FooBar
+        If X is not multiple of 3 or 5, print Qix*/
+        
+        IF x is multiple by 3 OUTPUT = "Foo"
+        Println(Foo)
+        
+        IF x is multiple by 5 OUTPUT = "Bar"
+        println(Bar)
+        
+        IF x is multiple by 3 & 5 OUTPUT = "FooBar"
+        println(FooBar)
+        
+        IF x is not multiple by 3 or 5 OUTPUT = "Qix"
+        println(Qix)
+    """
+}
+fun main() {
+
 }
