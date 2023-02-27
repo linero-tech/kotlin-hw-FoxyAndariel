7c121a7e16fafbd060ddf3a4cc22ccf447dac02c
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-27 21:08:11 +0100

    task10 module 101

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,14 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT user email
+        INPUT user password, must be six characters long or longer
+        IF email is mal-formed 
+        OUTPUT email error message
+        ELSE
+        Create user account
+        
+        
+    """
 }
