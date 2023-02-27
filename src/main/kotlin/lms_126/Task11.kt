c6d7096c0be34cae1536056cc09d66bc981933ec
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-27 21:59:01 +0100

    Task11 Module 101

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        IF unit is Celsius separate the unit from the magnitude
+        IF unit is Fahrenheit separate the unit from the magnitude
+        
+        IF C convert to F
+        ELSE
+        IF F convert to C
+        OUTPUT result of conversion 
+    """
 }
