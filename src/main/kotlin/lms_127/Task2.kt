2a562e224bb6eae204e443830fdd3552b1975447
FoxyAndariel <jonathan.studying@gmail.com>
2023-02-28 11:20:32 +0100

    task2 module 102 + "practice of  print statements""

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -13,4 +13,21 @@ fun task2() {
 }
 fun main(){
      task2()
+
+    println("My name is Jonathan")
+    println("My country is Sweden")
+    println("I am 180 centimeters tall")
+
+    print("> Enter your name: ")
+    val name = readLine()
+
+    print("> Enter your country: ")
+    val country = readLine()
+
+
+    // Option 2: String templates
+    println("Hello $name, you come from $country")
+
+
+
 }
