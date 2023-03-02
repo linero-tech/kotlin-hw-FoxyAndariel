ea561b6adb04f6ad12b801c00852bb623a5ebd17
FoxyAndariel <jonathan.studying@gmail.com>
2023-03-02 21:40:09 +0100

    task 5 module 102

--- src/main/kotlin/lms_127/Task5.kt
+++ src/main/kotlin/lms_127/Task5.kt
@@ -5,8 +5,17 @@ fun task5(valueForA: Int, valueForB: Int): Pair<Int, Int> {
     var a = valueForA
     var b = valueForB
 
-    TODO("Your solutions goes in here.")
+     val temporary = a
+    a = b
+    b = temporary
+    
 
     // Do not erase or change this statement
     return Pair(a, b)
+
+}
+
+fun main(){
+    println(task5(valueForA = 10, valueForB = 30 ))
+
 }
