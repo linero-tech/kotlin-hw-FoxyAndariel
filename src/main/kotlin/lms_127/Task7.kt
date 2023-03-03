8da6a6629ba085fdc411c32231ee41b4aec5fd78
FoxyAndariel <jonathan.studying@gmail.com>
2023-03-03 11:33:09 +0100

    task7 module 102

--- src/main/kotlin/lms_127/Task7.kt
+++ src/main/kotlin/lms_127/Task7.kt
@@ -1,6 +1,17 @@
 package lms_127
 
 fun task7(sentence: String): Int {
+    val result = sentence.length
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+
+fun main(){
+    println(task7(sentence = "I love GBG")) //10
+    println(task7(sentence = "")) //0
+    println(task7(sentence = "1234 1234")) //9
+    println(task7(sentence = "I")) //1
+    println(task7(sentence = "I love Gothenburg")) //17
+
+
 }
