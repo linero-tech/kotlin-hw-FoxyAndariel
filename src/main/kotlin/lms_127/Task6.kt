7847bf2afea101d710b806fbd62e62f7cf2c7587
FoxyAndariel <jonathan.studying@gmail.com>
2023-03-03 14:23:32 +0100

    Update Task6.kt

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -3,7 +3,7 @@ package lms_127
 fun task6(base1: Double, base2: Double, height: Double): Double {
      // base1 = 10.0, base2 = 20.0 &  height = 1.0
 
-    return(base1 +  base2) * height * 0.5
+    return(base1 + base2) * height * 0.5
 }
 
 fun main() {
