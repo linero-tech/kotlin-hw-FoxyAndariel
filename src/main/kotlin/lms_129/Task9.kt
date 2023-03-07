c951776a3720c3c70f2bac4f22bd86983f939e76
FoxyAndariel <jonathan.studying@gmail.com>
2023-03-07 19:04:20 +0100

    task9 module 103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,18 @@
 package lms_129
 
 fun task9(temperature: String): String {
+    val magnitude = temperature.substring(0, temperature.length - 1).toDouble()
+    val unit = temperature.last().uppercase()
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val convertedTemp = if (unit =="F") {
+     (magnitude * 9 / 5 + 32 ).toInt()
+    } else {
+        ((magnitude - 32) * 5 / 9).toInt()
+        20
+
+    }
+
+    val convertedUnit = if (unit== "C") "F" else "C"
+
+    return "$convertedTemp$convertedUnit"
 }
