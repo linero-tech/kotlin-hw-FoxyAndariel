39d53bbc56962b1d2bdd1c7e7166939b8fccec37
FoxyAndariel <jonathan.studying@gmail.com>
2023-03-07 17:48:38 +0100

    task8 Module 103

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -1,6 +1,19 @@
 package lms_129
 
 fun task8(number: Int): Int {
+      var result = 0
+    var remainingNumber = number
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    while(remainingNumber > 0) {
+        result += remainingNumber %10
+        remainingNumber /= 10
+        println("result = $result")
+        println("Remaining = $number")
+    }
+    return result
+}
+
+fun main() {
+    println(task8(number = 123))
+
 }
